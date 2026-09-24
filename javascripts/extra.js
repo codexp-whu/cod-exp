d4efa819badab39ac1647b9cfaa0f1842958b7a3
zihaojf <1845037747@qq.com>
2026-09-24 17:34:35 +0800

    Deployed 73befbb with MkDocs version: 1.6.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,4 +1,6 @@
 (function () {
+    console.log("extra.js loaded");
+
     // 加载 GoatCounter 统计脚本
     const script = document.createElement("script");
 
@@ -8,6 +10,14 @@
     script.async = true;
     script.src = "https://gc.zgo.at/count.js";
 
+    script.onload = function () {
+        console.log("GoatCounter count.js loaded");
+    };
+
+    script.onerror = function (error) {
+        console.error("GoatCounter count.js FAILED:", error);
+    };
+
     document.head.appendChild(script);
 
     // 显示累计访问量
@@ -15,6 +25,8 @@
         "https://codexp-whu.goatcounter.com/counter/TOTAL.json"
     )
         .then(response => {
+            console.log("GoatCounter status:", response.status);
+
             if (!response.ok) {
                 throw new Error(`HTTP ${response.status}`);
             }
@@ -22,8 +34,12 @@
             return response.json();
         })
         .then(data => {
+            console.log("GoatCounter data:", data);
+
             const counter =
                 document.querySelector("#visitor-count");
+
+            console.log("visitor element:", counter);
 
             if (counter) {
                 counter.textContent = data.count;
