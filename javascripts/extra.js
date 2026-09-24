9b0adc5186506d981a03d1f56a5933675a316bbf
zihaojf <1845037747@qq.com>
2026-09-24 17:22:53 +0800

    Deployed 73befbb with MkDocs version: 1.6.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,12 +1,21 @@
+console.log("extra.js loaded");
+
 fetch("https://codexp-whu.goatcounter.com/counter/TOTAL.json")
-    .then(response => response.json())
+    .then(response => {
+        console.log("GoatCounter status:", response.status);
+        return response.json();
+    })
     .then(data => {
+        console.log("GoatCounter data:", data);
+
         const el = document.querySelector("#visitor-count");
+
+        console.log("visitor element:", el);
 
         if (el) {
             el.textContent = data.count;
         }
     })
     .catch(error => {
-        console.error("无法获取访问量:", error);
+        console.error("GoatCounter error:", error);
     });
